8ca7186a2ae3f738cefe14327e6c7598afdf6db2
lauramargar <lauram@empathy.co>
2022-10-31 12:30:40 +0100

    filtros correctos

--- www/js/src.js
+++ www/js/src.js
@@ -17,22 +17,22 @@ async function nextPag(num){
 }
 
 async function findByPage(query) {
-    let statusFilter = document.getElementById('status-select').value;
+    /*let statusFilter = document.getElementById('status-select').value;
     let speciesFilter = document.getElementById('species-select').value;
-    console.log(statusFilter);
+    console.log(statusFilter);*/
 
-    if(statusFilter === ''||speciesFilter === '') {
+    if(filtro === false) {
         let name = document.getElementById('mySearch').value;
         if(name === null){
             name = ' ';
         }
-        console.log(statusFilter);
+        //console.log(statusFilter);
         const response = await fetch('https://rickandmortyapi.com/api/character/?page='+query+'&name='+name, {});
         const data = await response.json();
         return data;
     } else {
-        console.log('hola');
-        console.log(statusFilter);
+        //console.log('hola');
+        //console.log(statusFilter);
         filterBy();
     }
     
@@ -40,10 +40,10 @@ async function findByPage(query) {
 
 let types = new Array();
 let valores = new Array();
-//let filtro = new Boolean(false);
+let filtro = new Boolean(false);
 
 async function filterBy(){
-    //filtro = true;
+    filtro = true;
     let statusFilter = document.getElementById('status-select');
     let speciesFilter = document.getElementById('species-select');
     let name = document.getElementById('mySearch').value;
@@ -74,7 +74,7 @@ async function filterBy(){
         charactersByFilter(types[0],valores[0],name,pag).then(all=>{
             print(all.results);
         });
-    }
+    } else {filtro = false;}
     
 }
 
